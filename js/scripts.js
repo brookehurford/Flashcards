fd05d96872d58bb80e067f482ec68981f35c9a1d
Brooke Hurford and Tyler Suderman <brookebhurford@gmail.com>
2016-01-11 16:19:54 -0800

    Add initial files, use jQuery and javascript

---
+++ js/scripts.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+  $("#card1").click(function() {
+    $("#js-open").toggle();
+    $("#js-closed").toggle();
+  });
+  $("#card2").click(function() {
+    $("#mod-open").toggle();
+    $("#mod-closed").toggle();
+  });
+  $("#card3").click(function() {
+    $("#op-open").toggle();
+    $("#op-closed").toggle();
+  });
+  $("#card4").click(function() {
+    $("#nan-open").toggle();
+    $("#nan-closed").toggle();
+  });
+  $("#card5").click(function() {
+    $("#var-open").toggle();
+    $("#var-closed").toggle();
+  });
+  $("#card6").click(function() {
+    $("#short-open").toggle();
+    $("#short-closed").toggle();
+  });
+  $("#card7").click(function() {
+    $("#func-open").toggle();
+    $("#func-closed").toggle();
+  });
+  $("#card8").click(function() {
+    $("#meth-open").toggle();
+    $("#meth-closed").toggle();
+  });
+  $("#card9").click(function() {
+    $("#arg-open").toggle();
+    $("#arg-closed").toggle();
+  });
+  $("#card10").click(function() {
+    $("#str-open").toggle();
+    $("#str-closed").toggle();
+  });
+  $("#card11").click(function() {
+    $("#methChain-open").toggle();
+    $("#methChain-closed").toggle();
+  });
+  $("#card12").click(function() {
+    $("#esc-open").toggle();
+    $("#esc-closed").toggle();
+  });
+  $("#card13").click(function() {
+    $("#alt-open").toggle();
+    $("#alt-closed").toggle();
+  });
+  $("#card14").click(function() {
+    $("#cc-open").toggle();
+    $("#cc-closed").toggle();
+  });
+});
